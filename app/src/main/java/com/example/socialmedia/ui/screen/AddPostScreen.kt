9850aacf7a16658228e92518008b8f77afc1bb91
Dimas A Anindyojati <anggorodimas@gmail.com>
2024-07-23 16:33:43 +0700

    add responsive addpost

--- app/src/main/java/com/example/socialmedia/ui/screen/AddPostScreen.kt
+++ app/src/main/java/com/example/socialmedia/ui/screen/AddPostScreen.kt
@@ -1,11 +1,13 @@
 package com.example.socialmedia.ui.screen
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -17,6 +19,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,46 +35,54 @@ fun AddPostScreen(sharedViewModel: SharedViewModel = viewModel()) {
     val postViewModel: PostViewModel = viewModel(factory = PostViewModelFactory(context.applicationContext))
 
     var content by rememberSaveable { mutableStateOf("") }
-
+    val configuration = LocalConfiguration.current
+    val isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
 
     Box(
         modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.TopStart
+        contentAlignment = if (isLandscape) Alignment.Center else Alignment.TopStart
     ) {
-        Column(
+        LazyColumn(
             horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
         ) {
-            Text(
-                text = "Add Post",
-                fontSize = 24.sp,
-                modifier = Modifier.padding(bottom = 16.dp)
-            )
-            username?.let {
+            item {
                 Text(
-                    text = "Username: $it",
-                    fontSize = 18.sp,
+                    text = "Add Post",
+                    fontSize = 24.sp,
                     modifier = Modifier.padding(bottom = 16.dp)
                 )
-            }
-            OutlinedTextField(
-                value = content,
-                onValueChange = { content = it },
-                label = { Text("Post Content") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(150.dp)
-                    .padding(bottom = 16.dp)
-            )
-            Button(onClick = {
                 username?.let {
-                    postViewModel.insertPost(it, content)
-                    content = ""
+                    Text(
+                        text = "Username: $it",
+                        fontSize = 18.sp,
+                        modifier = Modifier.padding(bottom = 16.dp)
+                    )
                 }
-            }) {
-                Text(text = "Submit")
+                OutlinedTextField(
+                    value = content,
+                    onValueChange = { content = it },
+                    label = { Text("Post Content") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(150.dp)
+                        .padding(bottom = 16.dp)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    onClick = {
+                        username?.let {
+                            postViewModel.insertPost(it, content)
+                            content = ""
+                        }
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Submit")
+                }
             }
         }
     }
-
 }
