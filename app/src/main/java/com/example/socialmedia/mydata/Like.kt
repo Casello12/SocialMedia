5af49537a606cd84d1dd75ae13e705fabf080048
Dimas A Anindyojati <anggorodimas@gmail.com>
2024-07-24 10:05:38 +0700

    add like

--- app/src/main/java/com/example/socialmedia/mydata/Like.kt
+++ app/src/main/java/com/example/socialmedia/mydata/Like.kt
@@ -1,11 +1,11 @@
 package com.example.socialmedia.mydata
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "likes")
 data class Like(
-    @PrimaryKey(autoGenerate = true) val postId: Int = 0,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val postId: Int,
     val username: String
 )
