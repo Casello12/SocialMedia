746e41b8d636da13eb43512d22579eb2553ac753
Dimas A Anindyojati <anggorodimas@gmail.com>
2024-07-24 11:10:49 +0700

    add eye open closed login

--- app/src/main/java/com/example/socialmedia/ui/screen/LoginScreen.kt
+++ app/src/main/java/com/example/socialmedia/ui/screen/LoginScreen.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,12 +18,17 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.socialmedia.MyApplication
+import com.example.socialmedia.R
 
 @Composable
 fun LoginScreen(
@@ -35,6 +42,7 @@ fun LoginScreen(
     var username by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
     var errorMessage by rememberSaveable { mutableStateOf("") }
+    var passwordVisible by rememberSaveable { mutableStateOf(false) }
 
     Box(
         modifier = modifier.fillMaxSize(),
@@ -62,6 +70,17 @@ fun LoginScreen(
                 value = password,
                 onValueChange = { password = it },
                 label = { Text("Password") },
+                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                trailingIcon = {
+                    val eyeIcon: Painter = if (passwordVisible) {
+                        painterResource(id = R.drawable.ic_eye_open) // Add your own icon
+                    } else {
+                        painterResource(id = R.drawable.ic_eye_closed) // Add your own icon
+                    }
+                    IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                        Icon(painter = eyeIcon, contentDescription = if (passwordVisible) "Hide password" else "Show password")
+                    }
+                },
                 modifier = Modifier.padding(bottom = 32.dp)
             )
 
